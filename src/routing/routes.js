348371fe9f7091d1b34e2152e1e706d0770cba58
Alexey Radziuk <radyukag@yandex.ru>
2020-06-15 21:46:43 +0300

    feat: add object with roots

---
+++ src/routing/routes.js
@@ -0,0 +1,34 @@
+function mainPage() {
+    const page = document.createElement('div');
+    const header = document.createElement('h2');
+    header.innerText = 'This is main page';
+    const list = document.createElement('ul');
+    list.innerHTML = '<li><a href="/statistics">Statistic page</a></li><li><a href="/game">Game page</a></li>';
+    page.append(header, list);
+    return page;
+}
+function statistics() {
+    const page = document.createElement('div');
+    const header = document.createElement('h2');
+    header.innerText = 'This is statistics page';
+    const list = document.createElement('ul');
+    list.innerHTML = '<li><a href="/">Main page</a></li><li><a href="/game">Game page</a></li>';
+    page.append(header, list);
+    return page;
+}
+function game() {
+    const page = document.createElement('div');
+    const header = document.createElement('h2');
+    header.innerText = 'This is game page';
+    const list = document.createElement('ul');
+    list.innerHTML = '<li><a href="/statistics">Statistic page</a></li><li><a href="/">Main page</a></li>';
+    page.append(header, list);
+    return page;
+}
+const roots = {
+    '/': mainPage(),
+    '/statistics': statistics(),
+    '/game': game(),
+}
+
+export default roots;
