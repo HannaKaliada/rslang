8f75378b8d6afa3d010804f73542f49fa64c865b
Alexey Radziuk <radyukag@yandex.ru>
2020-06-16 16:11:18 +0300

    refactor: change structure of Routing class

--- src/index.js
+++ src/index.js
@@ -9,14 +9,5 @@ import routes from './routing/routes';
 const routing = new Routing(routes);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.querySelector('.root');
-    routing.navigation('#/');
-    window.onpopstate = () => {
-        if (!window.location.href.match(/#\/\w*$/)) {
-            return
-        }
-        const route = window.location.href.match(/#\/\w*$/)[0];
-        console.log(route);
-        routing.navigation(route);
-    };
+  routing.init();
 });
--- src/routing/routing.js
+++ src/routing/routing.js
@@ -3,10 +3,24 @@ class Routing {
     this.routes = routes;
   }
 
-  navigation(pathname) {
-    const root = document.querySelector('.root');
-    root.innerHTML = '';
-    root.append(this.routes[pathname]);
+  loadPage(pathname) {
+    this.root.innerHTML = '';
+    this.root.append(this.routes[pathname]);
+  }
+
+  navigation() {
+    if (!window.location.href.match(/#\/\w*$/)) {
+      return;
+    }
+    const route = window.location.href.match(/#\/\w*$/)[0];
+    this.loadPage(route);
+  }
+
+  init() {
+    this.root = document.querySelector('.root');
+    window.history.replaceState({}, '#/', `${window.location.origin}#/`);
+    this.navigation('#/');
+    window.onpopstate = this.navigation.bind(this);
   }
 }
 
