39307cfc323c15e7f10b9ba89cb5e44473a4131e
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-06-17 20:56:40 +0300

    feat:divided into modules, add .js and .scss file

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import '@/styles/normalize.css';
 import '@/styles/style.scss';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import '@/components/about-team-page/about-team-page.js';
 import Routing from './routing/routing';
 import routes from './routing/routes';
 
 const routing = new Routing(routes);
 
 document.addEventListener('DOMContentLoaded', () => {
-  routing.init();
+    routing.init();
 });
