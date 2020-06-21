82eb664f7d84ded083296c889e0ffc017be824f3
GlebZhidovich <gzhidovich@gmail.com>
2020-06-21 21:32:39 +0300

    feat: add words files

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/extensions
 import settings from '../components/settings/settings.js';
-import createStartPage from '../components/mini game/speak-it/start-page';
 
 function mainPage() {
   const page = document.querySelector('.root');
@@ -9,8 +8,7 @@ function mainPage() {
   const list = document.createElement('ul');
   list.innerHTML = `<li><a href="#/statis-tics">Statistic page</a></li>
   <li><a href="#/game">Game page</a></li>
-  <li><a href="#/settings">Settings</a></li>
-  <li><a href="#/speak-it">Speak it</a></li>`;
+  <li><a href="#/settings">Settings</a></li>`;
   page.append(header, list);
 }
 
@@ -36,18 +34,11 @@ function game() {
   page.append(header, list);
 }
 
-function speakIt() {
-  const page = document.querySelector('.root');
-  page.innerHTML = '';
-  page.append(createStartPage(page));
-}
-
 const routes = {
   '#/': mainPage,
   '#/statis-tics': statistics,
   '#/game': game,
   '#/settings': settings,
-  '#/speak-it': speakIt,
 };
 
 export default routes;
