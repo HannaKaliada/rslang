81c7296f9616c3acc7ea50d2741dd4de15012a41
HannaKaliada <kaledaania2411@gmail.com>
2020-06-23 11:57:10 +0300

    fix: hide warning and success notifications of settings were changed

--- src/components/settings/changeLimits.js
+++ src/components/settings/changeLimits.js
@@ -1,6 +1,8 @@
 export default function changeLimits() {
   document.querySelector('.limits').addEventListener('input', (event) => {
     if (event.target.classList.contains('change-limit')) {
+      document.querySelector('.success-notification').classList.add('hidden');
+      document.querySelector('.warning').classList.add('hidden');
       if (event.target.classList.contains('words-limit')) {
         document.querySelector('.words-limit-output').textContent = event.target.value;
       } else {
--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -2,9 +2,11 @@ import changeLimits from './changeLimits';
 import renderContent from './renderContent';
 import './settings.scss';
 import saveSettings from './saveSettings';
+import hideSuccessNotification from './hideSuccessNotification';
 
 export default function settings() {
   renderContent();
   changeLimits();
   saveSettings();
+  hideSuccessNotification();
 }
