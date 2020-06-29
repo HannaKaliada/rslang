cd7ec6abbbd9c8145cf3e6550113d9127f72bee0
HannaKaliada <kaledaania2411@gmail.com>
2020-06-29 12:04:37 +0300

    fix: change input size

--- src/components/learningWords/goToTheNextWord.js
+++ src/components/learningWords/goToTheNextWord.js
@@ -3,12 +3,17 @@ import setInputWidth from './setInputWidth';
 
 export default function goToTheNextWord() {
   document.querySelector('.input-top-layer').classList.remove('transparent');
+
   document.querySelector('.sentence').textContent = '';
   document.querySelector('.sentence').insertAdjacentHTML('beforeend', properties.words[localStorage.currentWord].textExample.replace(/\<.*\>/, '<input type="text" class="form-control word-input" maxlength="30"><span class="input-top-layer hidden"></span>'));
+
   [properties.missingWord] = properties.words[localStorage.currentWord].textExample.match(/(?<=\>).*(?=\<)/);
+
   document.querySelector('.sentence-translation').textContent = properties.words[localStorage.currentWord].textExampleTranslate;
   document.querySelector('.word__translation').textContent = properties.words[localStorage.currentWord].wordTranslate;
-  const input = document.querySelector('form input');
+
   setInputWidth();
+
+  const input = document.querySelector('form input');
   input.focus();
 }
--- src/components/learningWords/setInputWidth.js
+++ src/components/learningWords/setInputWidth.js
@@ -1,8 +1,6 @@
+import properties from './properties';
+
 export default function setInputWidth() {
   const input = document.querySelector('form input');
-  input.addEventListener('input', () => {
-    if (document.querySelector('.input-top-layer').textContent === '') {
-      input.style.width = `${((input.value.length + 1) * 8) + 24}px`;
-    }
-  });
+  input.style.width = `${((properties.missingWord.length + 1) * 8) + 24}px`;
 }
