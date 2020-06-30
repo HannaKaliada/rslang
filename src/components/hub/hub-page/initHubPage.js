75a9725b10af53e5b4edb6abb6f57ae05670b54c
mayinmarch <marfa.astafieva@gmail.com>
2020-07-01 02:37:35 +0300

    feat: add logout button handler

--- src/components/hub/hub-page/initHubPage.js
+++ src/components/hub/hub-page/initHubPage.js
@@ -3,8 +3,10 @@ import renderHeader from '../../../shared/header/renderHeader';
 import renderHubPage from './renderHubPage';
 import '../../../styles/header.scss';
 import '../../../styles/hub-page.scss';
+import logoutButtonHandler from '../../../shared/header/logoutButtonHandler';
 
 export default function initHubPage() {
   renderHeader();
   renderHubPage(info);
+  logoutButtonHandler();
 }
