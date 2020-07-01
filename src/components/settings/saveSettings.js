529784e2581740d05faf9f61db6e5e73f085270d
HannaKaliada <kaledaania2411@gmail.com>
2020-07-01 17:13:00 +0300

    feat: add additional settings

--- src/components/settings/saveSettings.js
+++ src/components/settings/saveSettings.js
@@ -1,20 +1,20 @@
 import saveSettingsToLocalStorage from './saveSettingsToLocalStorage';
 
 export default function saveSettings() {
-  saveSettingsToLocalStorage();
-
   document.querySelector('.save-btn').addEventListener('click', () => {
-    const checkboxes = document.querySelector('.info-settings').querySelectorAll('input');
+    const checkboxes = document.querySelector('.main-settings').querySelectorAll('input');
+    const warning = document.querySelector('.warning');
+    const success = document.querySelector('.success-notification');
 
     if ([...checkboxes].some((checkbox) => checkbox.checked)) {
-      document.querySelector('.warning').classList.add('hidden');
+      warning.classList.add('hidden');
 
       saveSettingsToLocalStorage();
 
-      document.querySelector('.success-notification').classList.remove('hidden');
+      success.classList.remove('hidden');
     } else {
-      document.querySelector('.success-notification').classList.add('hidden');
-      document.querySelector('.warning').classList.remove('hidden');
+      success.classList.add('hidden');
+      warning.classList.remove('hidden');
     }
   });
 }
