856152a5ab3198452100a5a5ca7bac9b128b2e79
Alexey Radziuk <radyukag@yandex.ru>
2020-07-08 20:11:32 +0300

    feat: prevent running audio several times

--- src/components/learningWords/correctAnswer.js
+++ src/components/learningWords/correctAnswer.js
@@ -2,13 +2,15 @@ import goToTheNextWord from './goToTheNextWord';
 import setWordsToProps from './setWordsToProps';
 import properties from './properties';
 
+function switchOffForm(e) {
+  e.stopImmediatePropagation();
+  e.preventDefault();
+}
+
 export default function correctAnswer() {
   const currentWord = localStorage.getItem('currentWord');
   const currentPage = localStorage.getItem('currentPage');
   const currentGroup = localStorage.getItem('currentGroup');
-
-  properties.currentWordPronunciation.play();
-  properties.currentWordExample.play();
 
   if (currentWord < 9) {
     localStorage.setItem('currentWord', Number(currentWord) + 1);
@@ -22,5 +24,16 @@ export default function correctAnswer() {
     }
     localStorage.setItem('currentWord', 0);
   }
-  goToTheNextWord();
+  properties.currentWordPronunciation.play();
+  const htmlDoc = document.querySelector('html');
+  htmlDoc.addEventListener('keydown', switchOffForm, true);
+  htmlDoc.addEventListener('click', switchOffForm, true);
+  properties.currentWordPronunciation.onended = () => {
+    properties.currentWordExample.play();
+  };
+  properties.currentWordExample.onended = () => {
+    goToTheNextWord();
+    htmlDoc.removeEventListener('keydown', switchOffForm, true);
+    htmlDoc.removeEventListener('click', switchOffForm, true);
+  };
 }
--- src/components/learningWords/setInputWidth.js
+++ src/components/learningWords/setInputWidth.js
@@ -1,6 +1,7 @@
 import properties from './properties';
 
 export default function setInputWidth() {
+  console.log(3);
   const input = document.querySelector('form input');
   input.style.width = `${((properties.missingWord.length + 1) * 8) + 24}px`;
   input.focus();
