30657498197d3679611ab9c29a12f5b314cdc52b
SemperIdem <faintler915@gmail.com>
2020-07-09 23:54:50 +0400

    feat: fix bug

--- src/components/mini-game/sprint/components/WordsSet.js
+++ src/components/mini-game/sprint/components/WordsSet.js
@@ -7,14 +7,19 @@ export default class WordsSet {
 
   getGamesPairWord() {
     let couple = new Object();
+    console.log("TEST " + this.collection[2].rus + " " +this.collection[2].eng);
     if (this.randomInteger(1,3) == 3) {
       let firstIndex = this.randomInteger(0,19);
       let secondIndex = this.randomInteger(0,19);
-      couple.rus = this.collection[this.randomInteger(0,19)].rus;
-      couple.eng = this.collection[this.randomInteger(0,19)].eng;
-      couple.correct = firstIndex != secondIndex ? false : true;
+      console.log("random " + firstIndex + ' and ' + secondIndex);
+      couple.rus = this.collection[firstIndex].rus;
+      couple.eng = this.collection[secondIndex].eng;
+      couple.correct = firstIndex !== secondIndex ? false : true;
+      console.log(couple.rus + " and " + couple.eng);
+      console.log(couple.correct);
     }
     else {
+      console.log('non-random');
       const index = this.randomInteger(0,19);
       couple.rus = this.collection[index].rus;
       couple.eng = this.collection[index].eng;
