a50ed8fc729f96afc502957010d99fc0bc9f1687
SemperIdem <faintler915@gmail.com>
2020-07-09 18:05:49 +0400

    feat: edit score

--- src/components/mini-game/sprint/pages/ResultPage.js
+++ src/components/mini-game/sprint/pages/ResultPage.js
@@ -9,7 +9,7 @@ export default function renderResultPage(score) {
   <div class="container vh-100 d-flex flex-column justify-content-center align-items-center">
   <div class="card w-50 h-50">
 <div class="card-body text-center">
-<div class="result">Congratulations!<br>  Your score: ${score}</div>
+<div class="result">Congratulations!<br>  Your score: ${score.score}</div>
 <div class="buttons mt-4">
   <button type="button" class="btn btn-primary btn-lg btn-again">Try again</button>
   <button type="button" class="btn btn-secondary btn-lg btn-menu">Choose another level</button>
