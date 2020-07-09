737d039a9d7528732d1fbf7c2aff8e0eed6472d6
HannaKaliada <kaledaania2411@gmail.com>
2020-07-09 23:14:40 +0300

    refactor: move games info to separate file

--- src/components/games-page/renderContent.js
+++ src/components/games-page/renderContent.js
@@ -1,3 +1,5 @@
+import games from "../../data/gamesInfo";
+
 export default function renderContent() {
   const page = document.querySelector('.root');
   page.insertAdjacentHTML('beforeend',
@@ -6,31 +8,6 @@ export default function renderContent() {
       <div class="games"></div>
     </div>`);
   const gamesContainer = document.querySelector('.games');
-  const games = [{
-    gameTitle: 'Speak It',
-    gameDesc: 'With the help of the game Speak It you will learn new words and also how to pronounce them correctly.',
-    gameURL: ''
-  },
-  {
-    gameTitle: 'English Puzzle',
-    gameDesc: 'The English Puzzle game will teach you to formulate sentences correctly and to put words in the right order.',
-    gameURL: ''
-  },
-  {
-    gameTitle: 'Savanna',
-    gameDesc: 'Savanna will make your vocabulary deeper and more diverse and also teaches you to think quickly.',
-    gameURL: '#/savanna-game',
-  },
-  {
-    gameTitle: 'Sprint',
-    gameDesc: '',
-    gameURL: ''
-  },
-  {
-    gameTitle: 'AudioCall',
-    gameDesc: '',
-    gameURL: '#/audiocall'
-  },]
   games.forEach((game) => {
   gamesContainer.insertAdjacentHTML('beforeend', `<div class="card game">
   <div class="game__title">${game.gameTitle}</div>
---
+++ src/data/gamesInfo.js
@@ -0,0 +1,27 @@
+const games = [{
+  gameTitle: 'Speak It',
+  gameDesc: 'With the help of the game Speak It you will learn new words and also how to pronounce them correctly.',
+  gameURL: ''
+},
+{
+  gameTitle: 'English Puzzle',
+  gameDesc: 'The English Puzzle game will teach you to formulate sentences correctly and to put words in the right order.',
+  gameURL: ''
+},
+{
+  gameTitle: 'Savanna',
+  gameDesc: 'Savanna will make your vocabulary deeper and more diverse and also teaches you to think quickly.',
+  gameURL: '#/savanna-game',
+},
+{
+  gameTitle: 'Sprint',
+  gameDesc: '',
+  gameURL: ''
+},
+{
+  gameTitle: 'AudioCall',
+  gameDesc: '',
+  gameURL: '#/audiocall'
+}]
+
+export default games;
