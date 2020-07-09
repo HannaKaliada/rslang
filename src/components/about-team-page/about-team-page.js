7ff340e6d01e28083450ab25ac00e040b47b2ef5
HannaKaliada <kaledaania2411@gmail.com>
2020-07-09 19:20:38 +0300

    feat: add container to about team page

--- src/components/about-team-page/about-team-page.js
+++ src/components/about-team-page/about-team-page.js
@@ -2,7 +2,7 @@ import data from '../../data/teamInfo.json';
 
 export default function renderAboutTeamPage() {
   const page = document.querySelector('.root');
-  const links = `<div class="about-team-wrapper">
+  const links = `<div class="container"><div class="about-team-wrapper">
     <div class="links"> <h1>About team page:</h1>
     <h1 ><a href="#/">Main</a></h1> </div>`;
   const start = `<div class="swiper-container">
@@ -11,7 +11,7 @@ export default function renderAboutTeamPage() {
     <div class="swiper-pagination"></div>
       <div class="swiper-button-next"></div>
      <div class="swiper-button-prev"></div>
-    </div>`;
+    </div></div>`;
   let content = '';
   data.forEach((el) => {
     content += `<div class="swiper-slide"><div class="card" >
