a81d7023b2623b888783194dec2e45f3d621bb73
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-07-10 18:57:31 +0300

    feat: change show message

--- src/components/learningWords/showMessage.js
+++ src/components/learningWords/showMessage.js
@@ -1,17 +1,32 @@
 export default function showMessage() {
   if (localStorage.getItem('currentWord') === localStorage.getItem('wordsLimit')) {
     const page = document.querySelector('.word');
-    const content = `<div class="alert alert-success" role="alert">
-    <h4 class="alert-heading">Well done!</h4>
-    <p>You have fulfilled your norm for today, you can stop or continue to learn new words</p>
-    <hr>
-    <p class="mb-0">To close message click close btn</p>
-    <button type="button" class="btn btn-success btn-close-show-message">Close</button>
-  </div>`;
+    const content = `<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>You have fulfilled your norm for today, you can stop or continue to learn new words.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary btn-close-show-message" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary btn-menu">Menu</button>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+
 
     page.insertAdjacentHTML('afterbegin', content);
-
     const btn = document.querySelector('.btn-close-show-message');
-    btn.addEventListener('click', () => document.querySelector('.alert').remove());
+    btn.addEventListener('click', () => document.querySelector('.modal').remove());
+    const btnMenu= document.querySelector('.btn-menu');
+    btnMenu.addEventListener('click',()=>document.location='#/hub');
   }
 }
