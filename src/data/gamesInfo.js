a123f09b8e582afff772c482083ccf4a4e29d7c5
HannaKaliada <kaledaania2411@gmail.com>
2020-07-12 22:51:31 +0300

    feat: add games description

--- src/data/gamesInfo.js
+++ src/data/gamesInfo.js
@@ -7,7 +7,7 @@ const games = [{
 {
   gameTitle: 'English Puzzle',
   gameDesc: 'The English Puzzle game will teach you to formulate sentences correctly and to put words in the right order.',
-  gameURL: '',
+  gameURL: '#/english-puzzle',
   image: require('../assets/images/puzzle.png'),
 },
 {
@@ -18,19 +18,19 @@ const games = [{
 },
 {
   gameTitle: 'Sprint',
-  gameDesc: '',
+  gameDesc: 'Sprint helps to learn new words and also to train memory and speed of thinking',
   gameURL: '#/sprint',
   image: require('../assets/images/sprint.png'),
 },
 {
   gameTitle: 'AudioCall',
-  gameDesc: '',
+  gameDesc: 'The AudioCall game helps to improve language perception and pronunciation of words.',
   gameURL: '#/audiocall',
   image: require('../assets/images/audiocall.png'),
 },
 {
   gameTitle: 'Team Game',
-  gameDesc: '',
+  gameDesc: 'The game develops the speed of thinking and quick memorization of words in context.',
   gameURL: '#/our-game',
   image: require('../assets/images/teamgame.png'),
 }];
