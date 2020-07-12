81c9f06f86072ddc502fbdced1d48b1aeed959c9
Alexey Radziuk <radyukag@yandex.ru>
2020-07-12 20:33:15 +0300

    fix: post settings method

--- src/services/postUserSettings.js
+++ src/services/postUserSettings.js
@@ -2,6 +2,7 @@ const postUserSettings = async (settings) => {
   const userInfo = JSON.parse(localStorage.getItem('userInfo'));
   await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userInfo.userId}/settings`, {
     method: 'PUT',
+    withCredentials: true,
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
